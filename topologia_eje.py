84391d5b8095734c3bfbca0dc534b965ca1dd9c5
Simranjit Singh <54179643+simranjit20001@users.noreply.github.com>
2022-04-10 15:50:22 +0200

    Add files via upload

---
+++ topologia_eje.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+
+from mininet.net import Mininet
+from mininet.node import Controller, RemoteController, OVSController
+from mininet.node import CPULimitedHost, Host, Node
+from mininet.node import OVSKernelSwitch, UserSwitch
+from mininet.node import IVSSwitch
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+from mininet.link import TCLink, Intf
+from subprocess import call
+
+def myNetwork():
+
+    net = Mininet( topo=None,
+                   build=False,
+                   ipBase='10.0.0.0/8')
+
+    info( '*** Adding controller\n' )
+    c0=net.addController(name='c0',
+                      controller=Controller,
+                      protocol='tcp',
+                      port=6633)
+
+    info( '*** Add switches\n')
+    s1 = net.addSwitch('s1', cls=OVSKernelSwitch)
+    s2 = net.addSwitch('s2', cls=OVSKernelSwitch)
+    s4 = net.addSwitch('s4', cls=OVSKernelSwitch)
+
+    r2 = net.addHost('r2', cls=Node, ip='0.0.0.0')
+    r2.cmd('sysctl -w net.ipv4.ip_forward=1')
+    r1 = net.addHost('r1', cls=Node, ip='0.0.0.0')
+    r1.cmd('sysctl -w net.ipv4.ip_forward=1')
+
+    info( '*** Add hosts\n')
+    h1 = net.addHost('h1', cls=Host, ip='10.0.0.1', defaultRoute=None)
+    h2 = net.addHost('h2', cls=Host, ip='10.0.0.2', defaultRoute=None)
+    h3 = net.addHost('h3', cls=Host, ip='10.0.0.3', defaultRoute=None)
+    h4 = net.addHost('h4', cls=Host, ip='10.0.0.4', defaultRoute=None)
+    h5 = net.addHost('h5', cls=Host, ip='10.0.0.5', defaultRoute=None)
+    h6 = net.addHost('h6', cls=Host, ip='10.0.0.6', defaultRoute=None)
+
+    info( '*** Add links\n')
+    net.addLink(h1, s1)
+    net.addLink(h2, s1)
+    net.addLink(s1, r1)
+
+    net.addLink(h3, s2)
+    net.addLink(h4, s2)
+    net.addLink(s2, r1)
+
+    net.addLink(h5, s4)
+    net.addLink(h6, s4)
+    net.addLink(s4, r2)
+
+    # net.addLink(r1, s6)
+    # net.addLink(s6, r2)
+
+    net.addLink(r1, r2)
+
+
+    info( '*** Starting network\n')
+    net.build()
+    info( '*** Starting controllers\n')
+    for controller in net.controllers:
+        controller.start()
+
+    info( '*** Starting switches\n')
+    net.get('s1').start([c0])
+    net.get('s2').start([c0])
+    net.get('s4').start([c0])
+
+   
+    CLI(net)
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel( 'info' )
+    myNetwork()
+
